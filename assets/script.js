6785823f03fd65a50acc1814c3380d4ca110a31e
kylezap <kzapcic+github@gmail.com>
2024-03-22 23:42:33 -0400

    write collectEmployee function

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,38 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  let employeesArray = []
+  let keepLooping = true;
+
+  while(keepLooping) {
+    
+    let employeeInfo = {
+        firstName: prompt("First Name") ,
+        lastName:  prompt("Last Name") ,
+        salary: prompt("Salary")
+  }
+  
+    employeesArray.push(employeeInfo)
+    let keepAdding = confirm("Would you like to continue?")
+    if (keepAdding == false) {
+        keepLooping = false;
+    }
+
+    console.log(employeesArray);
+
+
 }
+return employeesArray;
+} 
+
+
+  
+
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+
 }
 
 // Select a random employee
